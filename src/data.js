6119e69138c45459ba48461e0e496c99a51c1e20
TPhong <dangthanhphong2209@gmail.com>
2023-07-22 18:23:33 +0700

    Food Menu

---
+++ src/data.js
@@ -0,0 +1,182 @@
+const Menu = [
+    {
+        id: 1,
+        title: 'Bánh mỳ thịt',
+        category: 'Breakfast',
+        price: 20.000,
+        img: './images/item-1.png',
+        desc: 'Bánh mỳ mềm dai kết hợp với thịt xông khói, pate và rau sống tươi ngon.',
+        },
+        
+        {
+        id: 2,
+        title: 'Bún chả',
+        category: 'Lunch',
+        price: 40.000,
+        img: './images/item-2.png',
+        desc: 'Món ăn đặc trưng của Hà Nội, gồm bún và chả lăn lên than hoa, kèm nước mắm chua ngọt và rau sống.',
+        },
+        
+        {
+        id: 3,
+        title: 'Phở bò',
+        category: 'Breakfast',
+        price: 35.000,
+        img: './images/item-3.png',
+        desc: 'Món ăn nổi tiếng toàn cầu, bao gồm bánh phở mềm và thịt bò chín thơm phức.',
+        },
+        
+        {
+        id: 4,
+        title: 'Gỏi cuốn',
+        category: 'Lunch',
+        price: 25.000,
+        img: './images/item-4.png',
+        desc: 'Bánh tráng cuốn tôm thịt, rau sống và bún tươi, ăn kèm nước mắm chua ngọt.',
+        },
+        
+        {
+        id: 5,
+        title: 'Cơm tấm',
+        category: 'Lunch',
+        price: 50.000,
+        img: './images/item-5.png',
+        desc: 'Cơm tấm thơm bùi kèm thịt heo, trứng, đậu hủ và dưa leo.',
+        },
+        
+        {
+        id: 6,
+        title: 'Bánh xèo',
+        category: 'Lunch',
+        price: 30.000,
+        img: './images/item-6.png',
+        desc: 'Bánh xèo giòn mỏng, nhân thịt, tôm và gia vị, ăn kèm rau sống và nước mắm.',
+        },
+        
+        {
+        id: 7,
+        title: 'Chả cá Lã Vọng',
+        category: 'dinner',
+        price: 60.000,
+        img: './images/item-7.png',
+        desc: 'Cá chẽm tươi tẩm ướp gia vị và chiên giòn, ăn kèm bún và rau sống.',
+        },
+        
+        {
+        id: 8,
+        title: 'Hủ tiếu Nam Vang',
+        category: 'Breakfast',
+        price: 30.000,
+        img: './images/item-8.png',
+        desc: 'Món hủ tiếu nước đậm đà, thịt heo, tôm và mỳ xào.',
+        },
+        
+        {
+        id: 9,
+        title: 'Bánh mỳ kẹp thịt',
+        category: 'Breakfast',
+        price: 15.000,
+        img: './images/item-9.png',
+        desc: 'Bánh mỳ thơm béo kẹp thịt nướng, pate và rau sống.',
+        },
+        
+        {
+        id: 10,
+        title: 'Bánh bèo',
+        category: 'Breakfast',
+        price: 10.000,
+        img: './images/item-10.png',
+        desc: 'Bánh bèo nhỏ, mềm, ăn kèm tôm khô, mỡ hành và gia vị.',
+        },
+        
+        {
+        id: 11,
+        title: 'Cá kho tộ',
+        category: 'Lunch/dinner',
+        price: 50.000,
+        img: './images/item-11.png',
+        desc: 'Cá kho nấu cùng nước mắm, đường, gia vị và ớt, thơm béo, ăn kèm cơm trắng.',
+        },
+        
+        {
+        id: 12,
+        title: 'Bánh tráng trộn',
+        category: 'Lunch',
+        price: 20.000,
+        img: './images/item-12.png',
+        desc: 'Bánh tráng xé nhỏ kèm đu đủ, chả cá, tôm và gia vị.',
+        },
+        
+        {
+        id: 13,
+        title: 'Nem rán',
+        category: 'Lunch',
+        price: 25.000,
+        img: './images/item-13.png',
+        desc: 'Nem cuốn nhỏ, giòn tan, nhân thịt và tôm, ăn kèm rau sống và nước mắm chua ngọt.',
+        },
+        
+        {
+        id: 14,
+        title: 'Mì Quảng',
+        category: 'Lunch',
+        price: 40.000,
+        img: './images/item-14.png',
+        desc: 'Mì gói xào với thịt heo, tôm, trứng và nước lèo đậm đà.',
+        },
+        
+        {
+        id: 15,
+        title: 'Bánh canh cua',
+        category: 'Lunch',
+        price: 35.000,
+        img: './images/item-15.png',
+        desc: 'Mì sợi trơn dai, có cua, tôm và gia vị, thơm béo ngon lành.',
+        },
+        
+        {
+        id: 16,
+        title: 'Xôi xéo',
+        category: 'Breakfast',
+        price: 20.000,
+        img: './images/item-16.png',
+        desc: 'Xôi mềm béo kèm với mè rang, đậu phụng và gà hấp.',
+        },
+        
+        {
+        id: 17,
+        title: 'Lẩu thái',
+        category: 'dinner',
+        price: 70.000,
+        img: './images/item-17.png',
+        desc: 'Lẩu cay nóng hổi với nhiều loại thịt, hải sản và rau thơm.',
+        },
+        
+        {
+        id: 18,
+        title: 'Gà nướng muối ớt',
+        category: 'dinner',
+        price: 55.000,
+        img: './images/item-18.png',
+        desc: 'Gà nướng mềm ngọt, gia vị đậm đà, ăn kèm muối ớt.',
+        },
+        
+        {
+        id: 19,
+        title: 'Mực nướng',
+        category: 'dinner',
+        price: 45,
+        img: './images/item-19.png',
+        desc: 'Mực tươi thơm nướng giòn, ăn kèm nước mắm chua ngọt.',
+        },
+        
+        {
+        id: 20,
+        title: 'Canh chua cá',
+        category: 'Lunch/dinner',
+        price: 40,
+        img: './images/item-20.png',
+        desc: 'Canh chua chua ngọt, có cá và rau thơm, ăn kèm cơm trắng.',
+        },
+];
+export default Menu;
